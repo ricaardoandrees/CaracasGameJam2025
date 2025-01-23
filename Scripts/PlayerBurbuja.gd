ab5ef8b646bf6ced8c177b6143a5b5d855143d15
Ricardo <ricaardoandresma@gmail.com>
2025-01-23 19:58:48 -0400

    Update PlayerBurbuja.gd

--- Scripts/PlayerBurbuja.gd
+++ Scripts/PlayerBurbuja.gd
@@ -16,29 +16,22 @@ func _process(delta):
 
 func _physics_process(delta: float) -> void:
 	 #Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	velocity.x = SPEED * Input.get_axis("izquierda", "derecha")
 	move_and_slide()
 	 #Handle jump.
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		move_and_slide()
 
 
-	move_and_slide()
-	
-#if Input.is_action_just_pressed("saltar") and is_on_floor():
-	#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 func _input(event):
 	if event.is_action_pressed("izquierda"):
 		DIRECTION = -1
 	if event.is_action_pressed("derecha"):
 		DIRECTION = 1
 		
-#if :
+
 	
-#else:
-	#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#IRECTION = -1
