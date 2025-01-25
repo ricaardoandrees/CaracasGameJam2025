586ee270d783ab7f3de2a6649623e73aa1ac0823
manuehhhhh <mamartinez.23@est.ucab.edu.ve>
2025-01-25 16:36:50 -0400

    inicio obstaculo

--- balla_burbuja.gd
+++ balla_burbuja.gd
@@ -38,8 +38,8 @@ func _physics_process(delta: float) -> void:
 	
 
 
-func _on_body_entered(body):
-	animated_sprite_2d.play("collision")
+#func _on_body_entered(body):
+	#animated_sprite_2d.play("collision")
 
 
 func _on_area_entered(area):
