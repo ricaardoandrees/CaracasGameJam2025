1a6dead795bc4d2ac6c641e6ad72c8648e163923
manuehhhhh <mamartinez.23@est.ucab.edu.ve>
2025-01-25 22:35:32 -0400

    costias

--- Scripts/enemigo.gd
+++ Scripts/enemigo.gd
@@ -5,16 +5,32 @@ extends Node2D
 @onready var ray_cast_izq = $killzone/RayCastIzq
 @onready var killzone = $killzone
 @onready var ray_cast_arr = $killzone/RayCastArr
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 
 var direction = 1
 
+
 func _process(delta):
 	if (ray_cast_dere.is_colliding()) or ( not ray_cast_dere_piso.is_colliding()):
 		direction = -1
+		animated_sprite_2d.flip_h = false
 	if (ray_cast_izq.is_colliding()) or ( not ray_cast_izq_piso.is_colliding()):
 		direction = 1
+		animated_sprite_2d.flip_h = true
 	position.x += delta * direction * 60
+
+	
+
+func animation_finished() -> void:
+	print("afdafdsaaaaa")
+	if animated_sprite_2d.animation == "muerte":
+		print("done attacking")
+
+
+
+	
+
 	
 	
--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,4 +1,5 @@
 extends Area2D
+@onready var animated_sprite_2d = $"../AnimatedSprite2D"
 
 
 func _ready():
@@ -7,9 +8,13 @@ func _ready():
 func _on_body_entered(body):
 	print(typeof(body))
 	if(body.name == "PlayerBurbuja"):
+		animated_sprite_2d.play("ataque")
 		body.muelto()
 		
 
 
 func _on_area_entered(area):
-	get_parent().queue_free()
+	animated_sprite_2d.play("muerte")
+
+
+		
